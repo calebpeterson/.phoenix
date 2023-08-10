a9d0a66e131a7f07f733d614ea3297fb232ff895
Caleb Peterson <caleb.peterson@zapier.com>
2023-08-10 09:31:48 -0400

    add CMD+M to maximize, isolate F10-to-list-windows into its own file

--- index.js
+++ index.js
@@ -179,19 +179,8 @@ require("lib/hotkeys/copilot.js");
 // require("lib/hotkeys/jump-to.js");
 require("lib/hotkeys/join-zoom-jam-room.js");
 require("lib/hotkeys/jumps.js");
+require("lib/hotkeys/maximize.js");
 require("lib/hotkeys/notes.js");
+require("lib/hotkeys/list-windows.js");
 
-// Development helper - F10 to dump a list of all window titles + app names to the logs
-keys.push(
-  new Key("f10", [], () => {
-    const screens = Screen.all();
-    screens.forEach((screen) => {
-      log(screen.identifier(), screen.frame());
-    });
-
-    const windows = Window.all({ visible: true });
-    windows.forEach((window) => {
-      log(window.title(), window.app().name(), window.hash(), window.frame());
-    });
-  })
-);
+Phoenix.notify("Configuration loaded");
